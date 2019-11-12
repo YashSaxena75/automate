e39b14038153ef72511f1e265aee4cbf286fab2b
Yash Saxena <35332582+YashSaxena75@users.noreply.github.com>
2019-11-13 00:01:46 +0530

    Add files via upload

--- login.py
+++ login.py
@@ -1,17 +1,29 @@
 from selenium import webdriver
 import time
+import optparse
 from selenium.webdriver.common.keys import Keys
 driver=webdriver.Firefox(executable_path="/root/Downloads/driver/geckodriver")
 driver.set_page_load_timeout(10)
-uname="test100"
-passw="2525"
+def parser():
+	parser=optparse.OptionParser()
+	parser.add_option("-u", "--user", dest="un", help="please specify user id")
+	parser.add_option("-p", "--security",dest="pa",help="please sepcify the password")
+	(options,arguments)=parser.parse_args()
+	if not options.un:
+		parser.error("[-] Please Specify the userid , Use --help for more info.")
+	if not options.pa:
+		parser.error("[-] Please Specify the password, Use --help for more info.")
+	return options
+
+options=parser()
+
 try:
 	driver.get("http://172.16.30.200:8090/httpclient.html")
-	driver.find_element_by_id("username").send_keys(uname)
-	driver.find_element_by_id("password").send_keys(passw)
+	driver.find_element_by_id("username").send_keys(options.un)
+	driver.find_element_by_id("password").send_keys(options.pa)
 	driver.find_element_by_xpath("//*[@id='loginbutton']").click()
-	driver.close()
-	print("Login successfull with username : " + uname + "and pssword " + passw)
+	print("Login successfull with username : " + options.un + " and password " + options.pa)
+
 except Exception as e:
 	print(e)
 
